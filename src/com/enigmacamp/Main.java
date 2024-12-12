0d325fc9cd0259b38b960a0addc02986d7c58df6
acep handika <acephandika27@gmail.com>
2024-12-12 16:11:16 +0700

    fix: validation index of bound and unique data

--- src/com/enigmacamp/Main.java
+++ src/com/enigmacamp/Main.java
@@ -3,13 +3,28 @@ package com.enigmacamp;
 public class Main {
     public static void main(String[] args) {
         Nasabah newNasabah = new Nasabah(1, "Juan", "1234567890", "082332234", "2009-01-01");
+        Nasabah newNasabah2 = new Nasabah(2, "Juan", "12345678900", "0823322340", "2009-01-01");
+        Nasabah newNasabah3 = new Nasabah(1, "Juan", "1234567890", "082332234", "2009-01-01");
+        Nasabah newNasabah4 = new Nasabah(1, "Juan", "1234567890", "082332234", "2009-01-01");
+        Nasabah newNasabah5 = new Nasabah(1, "Juan", "1234567890", "082332234", "2009-01-01");
+        Nasabah newNasabah6 = new Nasabah(1, "Juan", "1234567890", "082332234", "2009-01-01");
+
+
         NasabahService service = new NasabahService();
 
+        //--------CREATE
         service.create(newNasabah);
+        service.create(newNasabah2);
+        service.create(newNasabah3);
+        service.create(newNasabah4);
+        service.create(newNasabah5);
+        service.create(newNasabah6);
 
-        for (Nasabah nasabah : service.getItems()){
-            if (nasabah != null) System.out.println(nasabah);
-        }
+        Nasabah updateNasabah = new Nasabah(3, "Acep", "1212121212", "082316167554", "2001-09-01");
+        service.update(1, updateNasabah);
+
+
+
     }
 
     // 1. Lengkapi service CRUD (Create, Read, Update, dan Delete)
--- src/com/enigmacamp/NasabahService.java
+++ src/com/enigmacamp/NasabahService.java
@@ -1,19 +1,62 @@
 package com.enigmacamp;
 
+import java.util.Arrays;
+
 //Untuk memanipulasi data nasabah seperti CRUD Nasabah
 public class NasabahService {
-    private Nasabah[] items= new Nasabah[15];
-    private int nasabahCOunt = 0;
+    private Nasabah[] items= new Nasabah[5];
+    private int nasabahCount = 0;
 
     public NasabahService(){}
 
     //Registrasi Nasabah
     public void create(Nasabah nasabah){
-        items[nasabahCOunt] = nasabah;
-        System.out.println("Nasabah berhasil ditambahkan");
+        try {
+            if (nasabahCount >= items.length){
+                throw new ArrayIndexOutOfBoundsException("Kapasitas Penuh");
+            }
+            if (!isUnik(nasabah.getId(), nasabah.getNik(), nasabah.getPhoneNumber())){
+                throw new Exception("Data sudah tersedia");
+            }
+            items[nasabahCount] = nasabah;
+            nasabahCount++;
+            System.out.println("Nasabah Berhasil Ditambahkan");
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+//        try {
+//            items[nasabahCOunt] = nasabah;
+//            System.out.println("Nasabah berhasil ditambahkan");
+//        }catch (ArrayIndexOutOfBoundsException e){
+//            System.out.println(e.getMessage());
+//        }
     }
+
+
 
     public Nasabah[] getItems() {
         return items;
+    }
+
+    private boolean isUnik(int id, String nik, String phoneNUmber){
+        for (Nasabah nasabah : items){
+            if (nasabah != null){
+                if (nasabah.getId()==id || nasabah.getNik().equals(nik) || nasabah.getPhoneNumber().equals(phoneNUmber)){
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "NasabahService{" +
+                "items=" + Arrays.toString(items) +
+                ", nasabahCount=" + nasabahCount +
+                '}';
     }
 }
